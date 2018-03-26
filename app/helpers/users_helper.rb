2902625be801e2570c07d01b1c0811b935d69a8c
Hannie Dong <hdong92@gmail.com>
2018-03-25 23:40:38 -0700

    Create users view.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   def guest_user(with_retry = true)
-    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user)
   end 
 
   private 
@@ -10,5 +10,5 @@ module UsersHelper
     user = User.create(user_agent: SecureRandom.urlsafe_base64(32), counter: 0, first_visit: Time.now, recent_visit: Time.now)
     session[:guest_user_id] = user.id
   end
-  
+
 end
